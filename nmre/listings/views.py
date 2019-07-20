1ce78e7a3f43922bb0fd3fbd6a345c5203e500bc
Hnicolas <nicolusmaluleke@gmail.com>
2019-07-20 20:34:14 +0200

    fxing the Search form

--- nmre/listings/views.py
+++ nmre/listings/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,get_object_or_404
 from .models import Listing
 from listings.choices import price_choices, bedroom_choices, state_choices
 
-# Create your views here.
+
 def  index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(listings, 6)
@@ -23,10 +23,33 @@ def  listing(request, listing_id):
     return render(request, 'listings/listing.html',context)
 
 def  search(request):
+    query_list = Listing.objects.order_by('-list_date')
     
+    #keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains=keywords)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__lte=bedrooms)
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_list = query_list.filter(price__lte=price)
+
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'query_list':query_list,
+        'values':request.GET
         }
     return render(request, 'listings/search.html', context)
